7e335e86ebfda0d174bdcd18a6e38446eb9a0554
Dani Gonzalez castillo <dax01_92@MacBook-Pro-de-Dani.local>
2014-08-12 16:35:02 +0200

    Initial Commit

---
+++ FiestasSAG/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FiestasSAG
+//
+//  Created by Dani Gonzalez castillo on 12/08/14.
+//  Copyright (c) 2014 Dani Gonzalez castillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FiestasSAGTests/FiestasSAGTests.m
@@ -0,0 +1,34 @@
+//
+//  FiestasSAGTests.m
+//  FiestasSAGTests
+//
+//  Created by Dani Gonzalez castillo on 12/08/14.
+//  Copyright (c) 2014 Dani Gonzalez castillo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FiestasSAGTests : XCTestCase
+
+@end
+
+@implementation FiestasSAGTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
